6eed628ca43f5f9df1846e6f20d8b3862fff839e
Bhavan Prakash <bhavanofficial2004@gmail.com>
2025-10-14 11:47:20 +0530

    Update Q2.java

--- src/Q2.java
+++ src/Q2.java
@@ -1,16 +1,16 @@
-//Print all prime numbers between 1 and 100 using nested loops.
 
-//public class Q2 {
-//    public static void main(String[] args) {
-//        for(int i = 0;i<=100;i++){
-//            if(i%1 == i && i%i == 1){
-//
-//            }
-//            for (int j = 0;j<=100;j++){
-//                if(j%j == 1 && j%1 == j){
-//                    System.out.println(j);
-//                }
-//            }
-//        }
-//    }
-//}
+
+public class Q2 {
+   public static void main(String[] args) {
+       for(int i = 0;i<=100;i++){
+           if(i%1 == i && i%i == 1){
+
+           }
+           for (int j = 0;j<=100;j++){
+               if(j%j == 1 && j%1 == j){
+                   System.out.println(j);
+               }
+           }
+       }
+   }
+}
